8459ed58d2daf417596f86902ff1e0ee11178849
wangwenjie <jancywen@163.com>
2020-05-17 17:47:12 +0800

    launch

---
+++ GameEngine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GameEngine
+//
+//  Created by wangwenjie on 2020/5/16.
+//  Copyright © 2020 wangwenjie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
